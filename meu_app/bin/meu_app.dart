3a8e71c406ad1e9276284f8ccb0dbba3cf6fb66a
Leonardo Fernandes <leonardofernandesmastroto@gmail.com>
2025-09-30 17:29:10 -0300

    atualizaçao no codigo da calculadora

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,85 +1,117 @@
 import 'dart:io';
 
 void main() {
-  print('\nCalculadora do dj');
-  
-  print('\nDigite o primeiro numero:');
-  int num1 = int.parse(stdin.readLineSync()!);
-  
-  String operacao;
-  bool operacaovalida = false;
-  
-  do {
-    print('\nDigite a operação (+, -, *, /):');
-    operacao = stdin.readLineSync()!;
-    
-    switch (operacao) 
-    {
-      
-      case "+":
-      print('\nDigite o segundo numero:');
-      int num2 = int.parse(stdin.readLineSync()!);
-        soma(num1, num2);
-        operacaovalida = true;
-        break;
-      
-      case "-":
-      print('\nDigite o segundo numero:');
-      int num2 = int.parse(stdin.readLineSync()!);
-        subtracao(num1, num2);
-        operacaovalida = true;
-        break;
-      
-      case "*":
-      print('\nDigite o segundo numero:');
-      int num2 = int.parse(stdin.readLineSync()!);
-        multiplicacao(num1, num2);
-        operacaovalida = true;
-        break;
-      
-      case "/":
-      print('\nDigite o segundo numero:');
-      int num2 = int.parse(stdin.readLineSync()!);
-        if (num2 == 0) 
-        {
-          print('Erro: Divisão por zero! Tente outro número.');
-        } 
-        
-        else 
-        {
-          divisao(num1, num2);
-          operacaovalida = true;
-        }
-        break;
+  bool continuar = true;
 
-      default:
-        print('Operação inválida! Digite apenas +, -, * ou /');
+  while (continuar) {
+    print('''\n ██████╗ █████╗ ██╗      ██████╗██╗   ██╗██╗      █████╗ ██████╗  ██████╗ 
+██╔════╝██╔══██╗██║     ██╔════╝██║   ██║██║     ██╔══██╗██╔══██╗██╔═══██╗
+██║     ███████║██║     ██║     ██║   ██║██║     ███████║██║  ██║██║   ██║
+██║     ██╔══██║██║     ██║     ██║   ██║██║     ██╔══██║██║  ██║██║   ██║
+╚██████╗██║  ██║███████╗╚██████╗╚██████╔╝███████╗██║  ██║██████╔╝╚██████╔╝
+ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ 
+                                                                          
+██████╗  █████╗     ██████╗  ██████╗     ██████╗      ██╗                 
+██╔══██╗██╔══██╗    ██╔══██╗██╔═══██╗    ██╔══██╗     ██║                 
+██████╔╝███████║    ██║  ██║██║   ██║    ██║  ██║     ██║                 
+██╔══██╗██╔══██║    ██║  ██║██║   ██║    ██║  ██║██   ██║                 
+██║  ██║██║  ██║    ██████╔╝╚██████╔╝    ██████╔╝╚█████╔╝                 
+╚═╝  ╚═╝╚═╝  ╚═╝    ╚═════╝  ╚═════╝     ╚═════╝  ╚════╝                  ''');
+
+print('''\n #########\                                                      #########\
+ #(=====)# |                           ///                       #(=====)# |
+ ######### |                         _(oo)_                      ######### |
+ #-------# |                        // \/ //                     #-------# |
+ #|  -  |# |          -- __________//____//__________--          #|  -  |# |
+ #|( O )|# |    --  --  /-/\-  -+/ -+- -+- /-/\-  -+/| --  --    #|( O )|# |
+ #|  -  |# |  --  --   / /( o ) / ^^^ /// / /( o ) / /   --  --  #|  -  |# |
+ #|-----|# |--        / &   - o/ ^^^ /// / &   - o/ /          --#|-----|# |
+ #########/          |#########|#########|########|/             #########/''');
+
+    int num1 = lerNumero('\nDigite o primeiro número:');
+
+    String operacao;
+    bool operacaoValida = false;
+
+    do {
+      print('\nDigite a operação (+, -, *, /):');
+      operacao = stdin.readLineSync()!;
+
+      int num2;
+
+      switch (operacao) {
+        case "+":
+          num2 = lerNumero('\nDigite o segundo número:');
+          soma(num1, num2);
+          operacaoValida = true;
+          break;
+
+        case "-":
+          num2 = lerNumero('\nDigite o segundo número:');
+          subtracao(num1, num2);
+          operacaoValida = true;
+          break;
+
+        case "*":
+          num2 = lerNumero('\nDigite o segundo número:');
+          multiplicacao(num1, num2);
+          operacaoValida = true;
+          break;
+
+        case "/":
+          num2 = lerNumero('\nDigite o segundo número:');
+          if (num2 == 0) {
+            print('Erro: Divisão por zero! Tente outro número.');
+          } else {
+            divisao(num1, num2);
+            operacaoValida = true;
+          }
+          break;
+
+        default:
+          print('Operação inválida! Digite apenas +, -, * ou /');
+      }
+    } while (!operacaoValida);
+
+   
+    print('\nDeseja usar a calculadora novamente? (s/n)');
+    String resposta = stdin.readLineSync()!.toLowerCase();
+    if (resposta != 's') {
+      continuar = false;
+      print('\nEncerrando a calculadora... Até mais!');
     }
-
-  } while (operacaovalida==false);
-
+  }
 }
 
 
+int lerNumero(String mensagem) {
+  while (true) {
+    print(mensagem);
+    String? entrada = stdin.readLineSync();
 
-
-
-void soma(num1, num2) 
-{
-  print('\nResultado da soma de $num1 + $num2 = ${num1 + num2}');
+    if (entrada != null) {
+      int? numero = int.tryParse(entrada);
+      if (numero != null) {
+        return numero;
+      }
+    }
+    print('Entrada inválida! Digite um número inteiro.');
+  }
 }
 
-void subtracao(num1, num2) 
-{
-  print('\nResultado da subtração de $num1 - $num2 = ${num1 - num2}');
+
+void soma(int num1, int num2) {
+  print('\nResultado: $num1 + $num2 = ${num1 + num2}');
 }
 
-void multiplicacao(num1, num2) 
-{
-  print('\nResultado da multiplicação de $num1 * $num2 = ${num1 * num2}');
+void subtracao(int num1, int num2) {
+  print('\nResultado: $num1 - $num2 = ${num1 - num2}');
 }
 
-void divisao(num1, num2) 
-{
-  print('\nResultado da divisão de $num1 / $num2 = ${num1 / num2}');
+void multiplicacao(int num1, int num2) {
+  print('\nResultado: $num1 * $num2 = ${num1 * num2}');
+}
+
+void divisao(int num1, int num2) {
+  print('\nResultado: $num1 / $num2 = ${num1 / num2}');
 }
